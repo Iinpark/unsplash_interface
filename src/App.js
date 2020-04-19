9f3170c96d3141123bbbeb9713d16710c1f402ec
Sultan Esbolatov <linpark11197@gmail.com>
2020-04-19 23:43:55 +0600

    Починили роутинг. Теперь при открытии сайта картинки грузятся сразу

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./App.css";
 import AppHeader from "./components/AppHeader/AppHeader";
 import { Store } from "./redux/store";
@@ -9,7 +9,7 @@ import RootRouter from "./navigation/RootRouter";
 function App() {
   return (
     <Provider store={Store}>
-      <BrowserRouter>
+      <HashRouter >
         <div className="header">
           <AppHeader />
         </div>
@@ -23,7 +23,7 @@ function App() {
             </a>
           </div>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
