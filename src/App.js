595d2db38c41f5872121f2f739eac1d0d6109777
Sultan Esbolatov <linpark11197@gmail.com>
2020-04-22 21:57:11 +0600

    Футер всегда внизу

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ import RootRouter from "./navigation/RootRouter";
 function App() {
   return (
     <Provider store={Store}>
-      <HashRouter >
-        <div className="header">
-          <AppHeader />
-        </div>
+      <HashRouter>
+        <div className="MainWrapper">
+          <div className="header">
+            <AppHeader />
+          </div>
 
-        <div className="content">
-          <RootRouter />
-          <div className="manifesto">
-            <span>2020</span>
-            <a href="https://youtu.be/A0FZIwabctw" title="Узнать что это значит" target='blank'>
-              ⁕Made on Earth for Humans⁕
-            </a>
+          <div className="content">
+            <RootRouter />
+            <footer className="footer">
+              <span>2020</span>
+              <a
+                href="https://youtu.be/A0FZIwabctw"
+                title="Узнать что это значит"
+                target="blank"
+              >
+                ⁕Made on Earth for Humans⁕
+              </a>
+            </footer>
           </div>
         </div>
       </HashRouter>
