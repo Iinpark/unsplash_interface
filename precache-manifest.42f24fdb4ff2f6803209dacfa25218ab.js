54236171657a6f8883d8a1e0e2263193c2fd660e
Sultan Esbolatov <linpark11197@gmail.com>
2020-04-23 18:53:47 +0600

    Updates

--- precache-manifest.42f24fdb4ff2f6803209dacfa25218ab.js
+++ precache-manifest.42f24fdb4ff2f6803209dacfa25218ab.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5850f45ab3ee95e8c8a38e9deffccd2d",
+    "revision": "c34eb7613bfbec42b8fa6560b74e80d8",
     "url": "/unsplash_interface/index.html"
   },
   {
-    "revision": "d949153a927038de5d1d",
+    "revision": "6527fe5d40c798f9d10b",
     "url": "/unsplash_interface/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "9a3ec1541120fb248fc3",
-    "url": "/unsplash_interface/static/css/main.a8439d64.chunk.css"
+    "revision": "f71473bc6df124314ba0",
+    "url": "/unsplash_interface/static/css/main.1cb7aaa4.chunk.css"
   },
   {
-    "revision": "d949153a927038de5d1d",
-    "url": "/unsplash_interface/static/js/2.c38a9252.chunk.js"
+    "revision": "6527fe5d40c798f9d10b",
+    "url": "/unsplash_interface/static/js/2.111adcba.chunk.js"
   },
   {
     "revision": "115c29c453b61995b5be0af8c45bd83e",
-    "url": "/unsplash_interface/static/js/2.c38a9252.chunk.js.LICENSE.txt"
+    "url": "/unsplash_interface/static/js/2.111adcba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a3ec1541120fb248fc3",
-    "url": "/unsplash_interface/static/js/main.485070b4.chunk.js"
+    "revision": "f71473bc6df124314ba0",
+    "url": "/unsplash_interface/static/js/main.1435865d.chunk.js"
   },
   {
     "revision": "4c8b371713d985655390",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unsplash_interface/precache-manifest.5aa2ca80c0a80de86576ebdfb5c6df7c.js"
+  "/unsplash_interface/precache-manifest.42f24fdb4ff2f6803209dacfa25218ab.js"
 );
 
 self.addEventListener('message', (event) => {
