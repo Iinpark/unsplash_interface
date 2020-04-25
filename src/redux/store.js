9f691adc50c8dce65ecf873aa60c4af8de7a23e6
Sultan Esbolatov <linpark11197@gmail.com>
2020-04-25 16:11:31 +0600

    Теперь состояние приложения сохраняется в localStorage. БАГ: если закрыть SPA а затем снова открыть - не подсвечиваются сердечки на лайкнутых фотках

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,35 @@ import thunk from "redux-thunk";
 import { searchReducer } from "./searchReducer";
 import { favoritesReducer } from "./favoritesReducer";
 import { photoReducer } from "./photoReducer";
+//redux-persist. for saving state into localStorage
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+
 
 const rootReducer = combineReducers({
   search: searchReducer,
   favorites: favoritesReducer,
   photos: photoReducer,
 });
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export let Store = createStore(
-  rootReducer,
-  composeEnhancer(applyMiddleware(thunk))
-);
+// export let Store = createStore(
+//   rootReducer,
+//   composeEnhancer(applyMiddleware(thunk))
+// );
+
+export default  () => {
+  let store = createStore(
+    persistedReducer,
+    composeEnhancer(applyMiddleware(thunk))
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
