2af4e7cf784a74cf15b65fc1ae0d2dec26a9841f
Sultan Esbolatov <linpark11197@gmail.com>
2020-04-29 18:17:38 +0600

    Updates

---
+++ precache-manifest.ac08d46682873cc14ecf1f2b9d97fd41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7556f10ce48f707c70767059b84258c6",
+    "url": "/unsplash_interface/index.html"
+  },
+  {
+    "revision": "f45e38f9282d39a41898",
+    "url": "/unsplash_interface/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ec8a884fb246deb33743",
+    "url": "/unsplash_interface/static/css/main.d70b182b.chunk.css"
+  },
+  {
+    "revision": "f45e38f9282d39a41898",
+    "url": "/unsplash_interface/static/js/2.ea4699c8.chunk.js"
+  },
+  {
+    "revision": "b93704ef8b7bcbf26a1736667cff3603",
+    "url": "/unsplash_interface/static/js/2.ea4699c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8a884fb246deb33743",
+    "url": "/unsplash_interface/static/js/main.9b3076e8.chunk.js"
+  },
+  {
+    "revision": "9d4a2565dc497f834126",
+    "url": "/unsplash_interface/static/js/runtime-main.49c64c64.js"
+  },
+  {
+    "revision": "ced1b14867b55ee83e66dd29b4c07f91",
+    "url": "/unsplash_interface/static/media/empty_placeholder.ced1b148.png"
+  },
+  {
+    "revision": "dca2961968c97d2983db0c68cd062804",
+    "url": "/unsplash_interface/static/media/instagram.dca29619.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resplash/precache-manifest.57546d509d5a1bb736a89a7be2d6b6d9.js"
+  "/unsplash_interface/precache-manifest.ac08d46682873cc14ecf1f2b9d97fd41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resplash/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unsplash_interface/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
